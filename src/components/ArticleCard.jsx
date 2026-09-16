2111e74f60ff82b39c7b581f3671dccc297bb71e
Mégane Buffa <meganebuffa@gmail.com>
2026-09-16 14:09:15 +0200

    fix: gérer les champs Contentful absents et les états de chargement

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 function ArticleCard({ slug, titre, chapo, auteur, image }) {
   return (
     <article>
-      <img src={image} alt="" width="400" />
+      {image && <img src={image} alt="" width="400" />}
       <h2>
         <Link to={`/article/${slug}`}>{titre}</Link>
       </h2>
-      <p>{chapo}</p>
-      <p>Par {auteur}</p>
+      {chapo && <p>{chapo}</p>}
+      {auteur && <p>Par {auteur}</p>}
     </article>
   )
 }
